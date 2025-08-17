b1f862707690df2c58997137076750a95967a0a9
Bradley Taylor <trenton.taylor.email@gmail.com>
2025-08-17 12:34:23 -0500

    2.5.0.2 Update

--- src/front/components/common/VersionPopup.jsx
+++ src/front/components/common/VersionPopup.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function VersionPopup({version}) {
     if (!version) return null;
-    return <div className="version-popup-centered">Version: {version} Author: Trenton Taylor</div>
+    return <div className="version-popup-centered">Version: {version}</div>
 }
 
 export default VersionPopup
